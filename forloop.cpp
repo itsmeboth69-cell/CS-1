83cd34aa398d9c65dc04838ceb05e2624599de38
itsmeboth69-cell <itsmeboth69@gmail.com>
2026-09-05 00:42:22 +0700

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+    int n;
+    int sum = 0;
+    int i;
+
+
+
+    cout << "Enter an integer:";
+    cin >> n;
+
+    
+
+    
+
+
+
+    for (int i = 0; i <= n; i++){
+
+
+        
+        sum += i;
+
+    
+
+        
+        
+
+
+    }
+
+    cout << "The sum of numbers from 1 to " << n << " is: " << sum << endl;
+
+
+
+    return 0;
+}
